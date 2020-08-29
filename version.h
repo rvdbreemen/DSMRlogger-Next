7297fde39f055055662f59bb69fb91e77a74fca8
Robert van den Breemen <robert@vandenbreemen.net>
2020-08-29 10:02:58 +0200

    fix bug in buildDataRecordFromSM

--- version.h
+++ version.h
@@ -2,13 +2,13 @@
 #define _VERSION_MAJOR 2
 #define _VERSION_MINOR 1  
 #define _VERSION_PATCH 2
-#define _VERSION_BUILD 359
+#define _VERSION_BUILD 367
 #define _VERSION_PRERELEASE rc2
-#define _VERSION_DATE "23-08-2020"
-#define _VERSION_TIME "23:20:32"
+#define _VERSION_DATE "24-08-2020"
+#define _VERSION_TIME "20:18:38"
 #define _SEMVER_CORE "2.1.2"
-#define _SEMVER_BUILD "2.1.2+359"
-#define _SEMVER_FULL "2.1.2-rc2+359"
-#define _SEMVER_NOBUILD "2.1.2-rc2 (23-08-2020)"
-#define _VERSION "2.1.2-rc2+359 (23-08-2020)"
+#define _SEMVER_BUILD "2.1.2+367"
+#define _SEMVER_FULL "2.1.2-rc2+367"
+#define _SEMVER_NOBUILD "2.1.2-rc2 (24-08-2020)"
+#define _VERSION "2.1.2-rc2+367 (24-08-2020)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
