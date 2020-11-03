463ea84ecc62ab6266ff71e83ae7bf1d1de0f1c2
Robert van den Breemen <robert@vandenbreemen.net>
2020-11-03 10:00:19 +0100

    fixed auto-discovery config

--- version.h
+++ version.h
@@ -2,13 +2,13 @@
 #define _VERSION_MAJOR 2
 #define _VERSION_MINOR 3  
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 501
+#define _VERSION_BUILD 502
 #define _VERSION_PRERELEASE rc5
 #define _VERSION_DATE "03-11-2020"
-#define _VERSION_TIME "01:05:06"
+#define _VERSION_TIME "07:49:12"
 #define _SEMVER_CORE "2.3.0"
-#define _SEMVER_BUILD "2.3.0+501"
-#define _SEMVER_FULL "2.3.0-rc5+501"
+#define _SEMVER_BUILD "2.3.0+502"
+#define _SEMVER_FULL "2.3.0-rc5+502"
 #define _SEMVER_NOBUILD "2.3.0-rc5 (03-11-2020)"
-#define _VERSION "2.3.0-rc5+501 (03-11-2020)"
+#define _VERSION "2.3.0-rc5+502 (03-11-2020)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
